2b242b1483386b1b6a4855087ef473e7039ac546
STM <legioner9>
2019-10-10 23:18:55 +0700

    3 vid_1

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 const express = require('express');
-const app = express();
-
+const server = express();
+let count = 0;
+server.get('/', (req, res) => {
+    console.log('Run' , `${count++}`);
+    res.send(`Hello You!))) , ${count++}`);
+})
+    .listen(3001);
