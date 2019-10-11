cdb50d9e2f4faf4dafa70ff5525ad5f03ffef9bc
STM <legioner9>
2019-10-11 14:21:37 +0700

    redirect vid_2

--- index.js
+++ index.js
@@ -21,7 +21,8 @@ let ech;
 server.post('/create', (req, res) => {
     console.log('Run', `${count++}`);
     ech = req.body.text;
-    arr.push(`${count} : ${ech} `);
-    res.render(`index`, {date_1: count, arr_1: arr});
+    arr.push(`${count} :: ${ech} `);
+    // res.render(`index`, {date_1: count, arr_1: arr});
+    res.redirect('/');
 });
 server.listen(3001);
