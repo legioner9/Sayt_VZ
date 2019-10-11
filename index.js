372cde4346363f699947f6d00770d11c08801052
STM <legioner9>
2019-10-11 09:08:19 +0700

    index.ejs vid_2

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const server = express();
+server.set('view engine', 'ejs');
+server.set('views', './src/views');
+
 let count = 0;
 server.get('/', (req, res) => {
-    console.log('Run' , `${count++}`);
-    res.send(`Hello You!))) , ${count++}`);
+    console.log('Run', `${count++}`);
+    console.log('{date_1 : count}' , {date_1 : count})
+    res.render(`index`,{date_1 : count});
 })
     .listen(3001);
