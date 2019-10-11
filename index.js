0ef1a9a5a507a0da3f0c1e39b1b69712d8abb184
STM <legioner9>
2019-10-11 13:55:08 +0700

    push vid_2

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
 const express = require('express');
+const bodyPars = require('body-parser');
+
 const server = express();
 server.set('view engine', 'ejs');
 server.set('views', './src/views');
+server.use(bodyPars.urlencoded({extended: true}));
 
 let count = 0;
+let arr = ['ast', 'geo', 'mix'];
+
 server.get('/', (req, res) => {
+    console.log('{date_1 : count}', {arr_1: arr})
+    res.render(`index`, {date_1: count, arr_1: arr});
+});
+
+server.get('/create', (req, res) => {
+    res.render(`create`, {date_1: count});
+});
+let ech;
+server.post('/create', (req, res) => {
     console.log('Run', `${count++}`);
-    console.log('{date_1 : count}' , {date_1 : count})
-    res.render(`index`,{date_1 : count});
-})
-    .listen(3001);
+    ech = req.body.text;
+    arr.push(`${count} : ${ech} `);
+    res.render(`index`, {date_1: count, arr_1: arr});
+});
+server.listen(3001);
