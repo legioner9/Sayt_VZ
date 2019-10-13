5b487098904f02ba7cf122d5800b5d19911f1e55
STM <legioner9>
2019-10-13 14:39:20 +0700

    link css and js vid_5"

--- src/app/app.js
+++ src/app/app.js
@@ -3,11 +3,13 @@ const bodyPars = require('body-parser');
 const config = require('../config/config');
 const database = require('../db/db');
 const Post = require('../db/models/post');
+const path = require('path');
 
 const server = express();
 server.set('view engine', 'ejs');
 server.set('views', './src/views');
 server.use(bodyPars.urlencoded({extended: true}));
+server.use('/public',express.static( 'public'));
 
 let count = 0;
 let arr = ['ast', 'geo', 'mix'];
