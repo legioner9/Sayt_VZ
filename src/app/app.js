b3d2a181011e55ef582532602d450c6213f3779e
STM <legioner9>
2019-10-13 21:43:31 +0700

    dropping create.js vid_5

--- src/app/app.js
+++ src/app/app.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const bodyPars = require('body-parser');
-const config = require('../config/config');
-const database = require('../db/db');
-const Post = require('../db/models/post');
-const path = require('path');
+// const config = require('../config/config');
+// const database = require('../db/db');
+// const Post = require('../db/models/post');
+// const path = require('path');
 
 const server = express();
 server.set('view engine', 'ejs');
@@ -12,30 +12,9 @@ server.use(bodyPars.urlencoded({extended: true}));
 server.use('/public', express.static('public'));
 server.use('/public', express.static('node_modules/jquery/dist'));
 
-let count = 0;
-let arr = ['ast', 'geo', 'mix'];
-
 server.get('/', (req, res) => {
-    Post.find({}).then(posts => {
-        res.render('index', {posts: posts});
-        // console.log('posts :: ', posts);
-    })
-    // res.render(`index`, {date_1: count, arr_1: arr});
+    res.render('index');
 });
 
-server.get('/create', (req, res) => {
-    res.render(`create`, {date_1: count});
-});
-// let ech;
-server.post('/create', (req, res) => {
-    let {title, body} = req.body;
-    // console.log(':::', title, body)
-    // arr.push(`${count} :: ${ech} `); ADD drop to DB
-    Post.create({title, body,})
-        .then(post => console.log('Success! post.id::', post.id))
-        .catch(err => console.error('err ::', err));
-    // res.render(`index`, {date_1: count, arr_1: arr}); instead this redirect('/')
-    res.redirect('/');
-});
 
 module.exports = server;
