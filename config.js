5f06ba187ebc17ed703576604dea6a804caad942
STM <legioner9>
2019-10-14 22:41:22 +0700

    step_3 dotenv .env error  vid_11

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
-// const dotenv = require('dotenv');
-// const path = require('path');
-//
-// const root = path.join.bind(this, __dirname);
-// dotenv.config({ path: root('.env') });
+const dotenv = require('dotenv');
+const path = require('path');
+
+const root = path.join.bind(this, __dirname);
+dotenv.config({ path: root('.env') });
 
 module.exports = {
     PORT: process.env.PORT || 3001,
-    mongoUrl: process.env.MongoUrl_1,
+    mongoUrl: process.env.MONGOUrl_1,
     IS_PRODUCTION: process.env.NODE_ENV === 'production',
 };
