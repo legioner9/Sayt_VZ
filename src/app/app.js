dff3d210afb0e95b4baa0bc4a39820e304c603c2
STM <legioner9>
2019-10-14 15:15:35 +0700

    end video static-asset hash NOT add ((( vid_9

--- src/app/app.js
+++ src/app/app.js
@@ -3,18 +3,22 @@ const bodyPars = require('body-parser');
 // const config = require('../config/config');
 // const database = require('../db/db');
 // const Post = require('../db/models/post');
-// const path = require('path');
+const staticAsset = require('static-asset');
+const path = require('path');
 
+process.rand_ = function () {
+    return Math.random().toString(36).slice(2)
+};
 const server = express();
 server.set('view engine', 'ejs');
 server.set('views', './src/views');
 server.use(bodyPars.urlencoded({extended: true}));
-server.use('/public', express.static('public'));
+server.use(staticAsset('node_modules/static-asset/lib'));
+server.use('/public', express.static(path.join('public')));
 server.use('/public', express.static('node_modules/jquery/dist'));
 
 server.get('/', (req, res) => {
     res.render('index');
 });
-
 
 module.exports = server;
